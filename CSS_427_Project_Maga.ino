a1e49eeecdf15dc3fc6121fa5d04a5f731caed70
Primxer <36143875+Primxer@users.noreply.github.com>
2020-11-25 14:54:05 -0800

    Manual sample and alt/tmp sensor

--- CSS_427_Project_Maga.ino
+++ CSS_427_Project_Maga.ino
@@ -6,50 +6,101 @@ static int VR1x = A0;
 static int VR1y = A1; //Joystick 1 y axis
 static int VR1sw = 22; //Joystick 1 button
 
-const bool TEST_MODE = true;// will print out data to serial monitor
+const bool TEST_MODE = false;// will print out data to serial monitor
+const int LOST_CONNECTION_TIME = 1000;
+bool sampleType = false;
+
+unsigned long lastReceiveTime = 0;
+unsigned long currentTime = 0;
+
+int altitude = 0;
+int tempC = 0;
 
 struct DATA_Package {
   byte VR1x_pos;
   byte VR1y_pos;
   byte VR1sw_val;
+  bool altRequest;
+  bool tempCRequest;
 };
 DATA_Package data;  //data is the package that will be sent via RF24
 
 RF24 radio(49, 48); // CE, CSN
-const byte address[6] = "00001";
+const byte addresses [][6] = {"00001", "00002"};
 
 void setup() { 
-  if(TEST_MODE){
-    Serial.begin(9600);
-  }
-  
+  Serial.begin(9600);
   //setup radio
   radio.begin();
-  radio.openWritingPipe(address);
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1, addresses[0]);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();  //do this before calling write()
 
   //initialise data
   data.VR1x_pos = 0;
   data.VR1y_pos = 0;
   data.VR1sw_val = 0;
+  data.altRequest = false;
+  data.tempCRequest = false;
 
   //pinMode
   pinMode(VR1x, INPUT);
   pinMode(VR1y, INPUT);
   pinMode(VR1sw, INPUT_PULLUP);
+
+  Serial.println("Which sample type? 1 = manual, 0 = periodic");
+  while(Serial.available() == 0);
+  char input = Serial.read();
+  if(input == '0') {
+    sampleType = true;
+    Serial.println("Enter Sampling Rate");
+  } else {
+    if(input != '1') {
+      Serial.println("Unknown input, defaulting to manual requests...");
+    }
+    Serial.println("Input 'y' into monitor for each request");
+  }
   
 }
 void loop() {
-  //read and set data
+
+  if(Serial.read()== 'y' && !sampleType) {
+    data.altRequest = true;
+  }
+  
+  //Send Radio Transmission
   data.VR1x_pos = map(analogRead(VR1x), 0, 1023, 0, 180);
   data.VR1y_pos = map(analogRead(VR1y), 0, 1023, 0, 180);
-  data.VR1sw_val = digitalRead(VR1sw);
+  delay(2);
+  radio.stopListening();
+  data.VR1sw_val = digitalRead(VR1sw); 
   
   radio.write(&data, sizeof(DATA_Package));
+  delay(2);
+
+  //Read Radio Transmission
+  radio.startListening();
+  if(radio.available()) {
+    if(data.altRequest) {
+      radio.read(&altitude, sizeof(altitude));
+      data.altRequest = false;
+      Serial.print(altitude); Serial.println(" meters");
+    }
+    if(data.tempCRequest) {
+      radio.read(&tempC, sizeof(altitude));
+      data.tempCRequest = false;
+    }
+
+    lastReceiveTime = millis();
+  }
 
   if(TEST_MODE){
     print_data();
+  }
+
+  currentTime = millis();
+  if ( currentTime - lastReceiveTime > LOST_CONNECTION_TIME ){
+    lostConnection();
   }
 }
 
@@ -59,5 +110,13 @@ void print_data(){
   Serial.print(", ");
   Serial.print(data.VR1y_pos);
   Serial.print(", ");
-  Serial.println(data.VR1sw_val);
+  Serial.print(data.VR1sw_val);
+  Serial.print(", ");
+  Serial.print(data.altRequest);
+  Serial.print(", ");
+  Serial.println(data.tempCRequest);
+}
+
+void lostConnection() {
+  return;
 }
--- CSS_427_Project_Uno.ino
+++ CSS_427_Project_Uno.ino
@@ -12,9 +12,10 @@
 unsigned long lastReceiveTime = 0;
 unsigned long currentTime = 0;
 
-const bool TEST_MODE = false; // will print out data to serial monitor
+const bool TEST_MODE = true; // will print out data to serial monitor
 const int LOST_CONNECTION_TIME = 1000; // last connected is over 1sec
-
+bool localAltRequest = false;
+bool localTempCRequest = false;
 
 Servo leftAileron; 
 Servo rightAileron;
@@ -23,12 +24,14 @@ Servo elevator;
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 
 RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
+const byte addresses [][6] = {"00001", "00002"};
 
 struct DATA_Package {
   byte VR1x_pos;
   byte VR1y_pos;
   byte VR1sw_val;
+  bool altRequest = false;
+  bool tempCRequest = false;
 };
 
 DATA_Package data;  //data is the package that will be read into via RF24
@@ -38,16 +41,15 @@ void setup() {
     Serial.begin(9600);
   }
 
-  Serial.begin(9600);
-  if(!baro.begin()) {
+  if(!baro.begin()) { //start the baromiter for altitude and tempurature 
   }
   
   //setup radio
   radio.begin();
-  radio.openReadingPipe(0, address);
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1, addresses[1]);
   radio.setPALevel(RF24_PA_MIN);
-  radio.startListening(); //do this before calling read()
-
+  
   leftAileron.attach(leftAileronPin);
   rightAileron.attach(rightAileronPin);
   elevator.attach(elevatorPin);
@@ -57,23 +59,39 @@ void setup() {
 }
 
 void loop() {
-  if (radio.available()) {
-    radio.read(&data, sizeof(DATA_Package));
 
+  //Read Radio transmission
+  delay(2);
+  radio.startListening();
+  if (radio.available()) {
+    while(radio.available()) {
+      radio.read(&data, sizeof(DATA_Package));
+    }
+    
     elevator.write(data.VR1y_pos);
     leftAileron.write(data.VR1x_pos);
     rightAileron.write(data.VR1x_pos);
-      
+
+    lastReceiveTime = millis();
+    
     if(TEST_MODE){
       print_data();
     }
   }
+  delay(2);
 
-  float altm = baro.getAltitude();
-  Serial.print(altm); Serial.println(" meters");
-
-  float tempC = baro.getTemperature();
-  Serial.print(tempC); Serial.println("*C");
+  //Send Radio Transmission
+  radio.stopListening();
+  if(data.altRequest) {
+      int altitude = baro.getAltitude();
+      radio.write(&altitude, sizeof(altitude));
+      Serial.println("sent alt");
+  }
+  if(data.tempCRequest) {
+    int tempC = baro.getTemperature();
+    radio.write(&tempC, sizeof(tempC));
+    Serial.println("sent tempC");
+  }
   
   currentTime = millis();
   if ( currentTime - lastReceiveTime > LOST_CONNECTION_TIME ){
@@ -87,7 +105,11 @@ void print_data(){
   Serial.print(", ");
   Serial.print(data.VR1y_pos);
   Serial.print(", ");
-  Serial.println(data.VR1sw_val);
+  Serial.print(data.VR1sw_val);
+  Serial.print(", ");
+  Serial.print(data.altRequest);
+  Serial.print(", ");
+  Serial.println(data.tempCRequest);
 }
 
 void lostConnection(){
