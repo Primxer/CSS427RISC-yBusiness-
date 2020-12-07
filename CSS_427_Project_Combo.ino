d0713b95b31268bfa51cc60e844d4debc5d0384c
Primxer <36143875+Primxer@users.noreply.github.com>
2020-12-07 01:07:43 -0800

    Periodic/Manual Samples, #of Packets, & ACK
    As we get more sensors added things will be added to the marked areas

--- CSS_427_Project_Combo.ino
+++ CSS_427_Project_Combo.ino
@@ -59,6 +59,8 @@ void setup() {
   radio.openWritingPipe(addresses[0]);
   radio.openReadingPipe(1, addresses[1]);
   radio.setPALevel(RF24_PA_MIN);
+  radio.setAutoAck(1);
+  radio.enableAckPayload();
   
   leftAileron.attach(leftAileronPin);
   rightAileron.attach(rightAileronPin);
@@ -77,15 +79,14 @@ void loop() {
   delay(2);
   radio.startListening();
   if (radio.available()) {
-    while(radio.available()) {
-      radio.read(&data, sizeof(DATA_Package));
-    }
-    
-    //elevator.write(data.VR1y_pos);
-    //leftAileron.write(data.VR1x1_pos);
-    //rightAileron.write(data.VR1x2_pos);
-    //rudder.write(data.VR2x_pos); 
-    //ESC.write(data.VR2y_pos);
+    radio.read(&data, sizeof(DATA_Package));
+
+    //Write to all the servos 
+    elevator.write(data.VR1y_pos);
+    leftAileron.write(data.VR1x1_pos);
+    rightAileron.write(data.VR1x2_pos);
+    rudder.write(data.VR2x_pos); 
+    ESC.write(data.VR2y_pos);
 
     lastReceiveTime = millis();
   }
@@ -93,17 +94,25 @@ void loop() {
 
   //Send Radio Transmission
   radio.stopListening();
-  if(data.altRequest) {
-    float altitude = baro.getAltitude();
-    radio.writeFast(&altitude, sizeof(altitude));
-    Serial.println("sent alt");
+  if(data.altRequest) { //THIS WILL NEED TO CHECK ALL SENSORS WITH || TO SEND DATA REQUEST ACK
+    char ackMessage[20] = "Data Request ACK";
+    radio.write(&ackMessage, sizeof(ackMessage));
   }
-  
+
+  //Check each sensor for request KEEP CHECKS IN ORDER BETWEEN LEADER/FOLLOWER
+  if(data.altRequest) {
+    //float altitude = baro.getAltitude();
+    //radio.write(&altitude, sizeof(altitude));
+  }
+
+  //ADD OTHER SENSOR LOGIC HERE
+
+
+  //Check if radio lost signal
   currentTime = millis();
   if ( currentTime - lastReceiveTime > LOST_CONNECTION_TIME ){
     lostConnection();
   }
-  print_data();
 }
 
 void print_data() {
@@ -144,10 +153,17 @@ static int VR2y = A3;
 static int VR1sw = 22; //Joystick 1 button
 
 const int LOST_CONNECTION_TIME = 1000;
+
 bool sampleType = false;
 
+int sampleRate = 0;
+unsigned long lastAltTime = 0;
 unsigned long lastReceiveTime = 0;
 unsigned long currentTime = 0;
+
+long int terminalCount = 0;
+long int leaderToFollowerCount = 0;
+long int followerToLeaderCount = 0;
 
 int altitude = 0;
 int tempC = 0;
@@ -156,7 +172,6 @@ DATA_Package data;  //data is the package that will be sent via RF24
 
 RF24 radio(49, 48); // CE, CSN
 const byte addresses [][6] = {"00001", "00002"};
-char ackMessage[] = "Data Request ";
 
 void setup() { 
   Serial.begin(9600);
@@ -166,6 +181,8 @@ void setup() {
   radio.openWritingPipe(addresses[1]);
   radio.openReadingPipe(1, addresses[0]);
   radio.setPALevel(RF24_PA_MIN);
+  radio.setAutoAck(1);
+  radio.enableAckPayload();
 
   //initialise data
   data.VR1x1_pos = 0;
@@ -181,23 +198,38 @@ void setup() {
   pinMode(VR2y, INPUT);
   pinMode(VR1sw, INPUT_PULLUP);
 
+  //User request for manual or periodic sample rate
   Serial.println("Which sample type? 1 = manual, 0 = periodic");
   while(Serial.available() == 0) {}
   char input = Serial.read();
   if(input == '0') {
     sampleType = true;
-    Serial.println("Enter Sampling Rate");
+    Serial.flush();
+    Serial.read();
+    Serial.println("Enter Sampling Rate (microseconds)");
+    Serial.flush();
+    Serial.read();
+    while(Serial.available() == 0) {}
+    sampleRate = Serial.parseInt();
+
+    //TODO: ADD SAMPLE RATE REQUESTS FOR EACH SENSOR
+    
   } else {
     if(input != '1') {
       Serial.println("Unknown input, defaulting to manual requests...");
     }
     Serial.println("Input 'y' into monitor for each request");
   }
+  terminalCount += 1;
 }
 void loop() {
 
+  //IF user input y and manual sample then send data request for ALL sensors 
   if(Serial.read()== 'y' && !sampleType) {
     data.altRequest = true;
+    Serial.println("Data Request Sent");
+    terminalCount+=1;
+    Serial.print("Terminal Input Count: "); Serial.println(terminalCount);
   }
 
   //Read Input
@@ -211,27 +243,50 @@ void loop() {
   //Send Radio Transmission
   delay(2);
   radio.stopListening();
-  data.VR1sw_val = digitalRead(VR1sw); 
-  
   radio.write(&data, sizeof(DATA_Package));
+  leaderToFollowerCount += 1;
   delay(2);
   //End Send Radio Transmission
 
   //Read Radio Transmission
   radio.startListening();
   if(radio.available()) {
+    //Collect ACK message if exists
+    radio.read(&data, sizeof(data));
+    char ackMessage[20] = "a";
+    radio.read(&ackMessage, sizeof(ackMessage));
+    Serial.println(ackMessage);
+    followerToLeaderCount += 1;
+    Serial.print("Follower->Leader Count: "); Serial.println(followerToLeaderCount);
+
+    //Reset all sensor requests and read sensor data from follower KEEP RESETS IN ORDER BETWEEN LEADER/FOLLOWER
     if(data.altRequest) {
-      radio.read(&altitude, sizeof(altitude));
+      //radio.read(&altitude, sizeof(altitude));
       data.altRequest = false;
-      Serial.print(altitude); Serial.println(" meters");
+      followerToLeaderCount += 1;
+      //Serial.print(altitude); Serial.println(" meters");
     }
+
+    //ADD SENSOR LOGIC HERE
+    
     lastReceiveTime = millis();
   }
   //End Read Radio Transmission
-  print_data();
+
+  //Check if radio has lost signal
   currentTime = millis();
+  
   if ( currentTime - lastReceiveTime > LOST_CONNECTION_TIME ){
     lostConnection();
+  }
+
+  //If in periodic mode check each sensor for if needed sample
+  //WILL ADD MORE TO THIS ONE AS WE GET MORE SENSORS WORKING
+  if(sampleType) {
+    if(currentTime - lastAltTime > sampleRate) {
+      data.altRequest = true;
+      lastAltTime = millis();
+    }
   }
 }
 
